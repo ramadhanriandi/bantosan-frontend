38029c5c502dae6e78a5bf158f2a7f2f0a35a29c
ramadhanriandi <izurbudianto@gmail.com>
2020-06-01 23:24:50 +0700

    init navbar view for non-authenticated user

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,36 @@
 <template>
   <div id="app">
-    <Map/>
-  </div>
-</template>
-
-<script>
-import Map from './components/Map.vue';
-
-export default {
-  name: 'app',
-  components: {
-    Map,
-  },
-};
-</script>
-
-<!-- <template>
-  <div id="app">
-    <div id="nav">
+    <Navbar />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
     <router-view/>
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #333333;
+  font-family: 'Rubik', sans-serif;
+  font-size: 12px;
+  font-weight: 400;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--->
---
+++ src/components/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light px-3">
+    <router-link class="navbar-brand mx-5" to="/">bantosan</router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbar-toggler-items"
+      aria-controls="navbar-toggler-items"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar-toggler-items">
+      <ul class="navbar-nav mr-auto">
+        <router-link class="nav-item mx-5 my-2" to="/disasters">
+          Disasters
+        </router-link>
+        <router-link class="nav-item mx-5 my-2" to="/fundraisings">
+          Fundraisings
+        </router-link>
+      </ul>
+      <div class="">
+        <router-link to="/login">
+          <button class="btn mx-5 my-2">Login</button>
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    activeMenu: String,
+    user: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+nav {
+  border-bottom: 1px solid #BFBFBF;
+  height: 80px;
+}
+
+.active {
+  font-weight: 500;
+}
+
+.btn {
+  background: #630FD1;
+  color: white;
+  height: 36px;
+  font-size: 12px;
+  font-weight: 500;
+}
+
+.nav-item {
+  color: #333333;
+  font-size: 14px;
+}
+
+.nav-item:hover {
+  text-decoration: none;
+}
+
+.navbar-brand, .navbar-brand:focus, .navbar-brand:hover {
+  color: #FF6565;
+  font-size: 24px;
+  font-weight: 700;
+}
+
+#navbar-toggler-items {
+  background: white;
+  text-align: right;
+}
+</style>
