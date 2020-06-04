98b315d4150fc422715a188be2e767a41fee7223
ramadhanriandi <izurbudianto@gmail.com>
2020-06-05 00:53:09 +0700

    add validation to register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,53 +1,83 @@
 <template>
-  <form class="border-wrapper w-50 p-5">
+  <form class="border-wrapper w-50 p-5" @submit="submitForm" method="post">
     <h1 class="title mb-2">Let's Get Started</h1>
     <p class="subtitle mb-4">Fill in the form below to register your account</p>
     <div class="form-group">
       <input
         type="text"
+        v-model="username"
         class="form-control p-3"
-        id="username"
+        :class="{ 'mb-4': !errors.username }"
         aria-describedby="usernameHelp"
         placeholder="Username"
+        required
       />
-      <small id="usernameHelp" class="form-text text-right">
-        Username already exists
+      <small
+        v-if="errors && errors.username"
+        id="usernameHelp"
+        class="form-text text-right"
+      >
+        {{ errors.username }}
       </small>
     </div>
     <div class="form-group">
       <input
         type="email"
+        v-model="email"
         class="form-control p-3"
-        id="email"
+        :class="{ 'mb-4': !errors.email }"
         aria-describedby="emailHelp"
         placeholder="Email"
+        required
       />
-      <small id="emailHelp" class="form-text text-right">
-        Invalid email format
+      <small
+        v-if="errors && errors.email"
+        id="emailHelp"
+        class="form-text text-right"
+      >
+        {{ errors.email }}
       </small>
     </div>
     <div class="form-group">
       <input
         type="password"
+        v-model="password"
         class="form-control p-3"
-        id="password"
+        name="password"
         aria-describedby="passwordHelp"
-        placeholder="Password"
+        placeholder="Password (min. 6 characters)"
+        required
       />
-      <small id="passwordHelp" class="form-text text-right">
-        Password must be at least 6 characters
+      <small
+        v-if="errors && errors.password"
+        id="passwordHelp"
+        class="form-text text-right"
+      >
+        {{ errors.password }}
       </small>
+      <password
+        v-model="password"
+        :secure-length="6"
+        :strength-meter-only="true"
+      />
     </div>
     <div class="form-group">
       <input
         type="password"
+        v-model="confirmPassword"
         class="form-control p-3"
-        id="confirmPassword"
+        :class="{ 'mb-4': !errors.confirmPassword }"
+        name="confirmPassword"
         aria-describedby="confirmPasswordHelp"
         placeholder="Re-type your password"
+        required
       />
-      <small id="confirmPasswordHelp" class="form-text text-right">
-        Password confirmation doesn't match
+      <small
+        v-if="errors && errors.confirmPassword"
+        id="confirmPasswordHelp"
+        class="form-text text-right"
+      >
+        {{ errors.confirmPassword }}
       </small>
     </div>
     <button class="btn my-2 w-100" type="submit">Sign up</button>
@@ -57,6 +87,49 @@
     </p>
   </form>
 </template>
+
+<script>
+import _ from 'lodash';
+import Password from 'vue-password-strength-meter';
+import validEmail from '@/assets/js/utils';
+
+export default {
+  components: { Password },
+  data: () => ({
+    errors: {},
+    username: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+  }),
+  methods: {
+    submitForm(e) {
+      this.errors = {};
+
+      if (this.username === 'existed_username') {
+        this.errors.username = 'Username already exists';
+      }
+      if (!validEmail(this.email)) {
+        this.errors.email = 'Invalid email format';
+      }
+      if (this.password.length < 6) {
+        this.errors.password = 'Password must be at least 6 characters';
+      }
+      if (this.password !== this.confirmPassword) {
+        this.errors.confirmPassword = 'Password confirmation doesn\'t match';
+      }
+
+      if (_.isEmpty(this.errors) && this.username && this.age && this.password) {
+        return true;
+      }
+
+      e.preventDefault();
+
+      return false;
+    },
+  },
+};
+</script>
 
 <style scoped lang="scss">
 @import '@/assets/scss/color.scss';
@@ -104,6 +177,10 @@
 
 .form-text {
   color: $RED;
+}
+
+.strength-meter {
+  margin: 0 0;
 }
 
 .subtitle {
