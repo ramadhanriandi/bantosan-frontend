8971dd48cd40d9b703a4fad6054ee1d17df5a408
ramadhanriandi <izurbudianto@gmail.com>
2020-06-04 21:01:21 +0700

    create new page for register view

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,9 @@ const routes = [
     component: () => import('../views/Home.vue'),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue'),
   },
 ];
 
