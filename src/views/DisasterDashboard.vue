ebd071335b69eaa69abb10b8d47fbc00ea386c13
ramadhanriandi <izurbudianto@gmail.com>
2020-06-15 06:09:30 +0700

    create view of reported disaster page for authenticated user

--- src/views/DisasterDashboard.vue
+++ src/views/DisasterDashboard.vue
@@ -2,24 +2,16 @@
   <div class="w-100">
     <div class="d-flex align-items-center mb-4 row">
       <div class="col-12 col-sm-12 col-lg-6">
-        <h2 class="title mb-2">Current Disasters in Indonesia</h2>
-        <p v-if="disasters.length > 0" class="subtitle mb-4 mb-sm-4 mb-lg-0">
-          Latest Update : {{ getLatestUpdate }}
+        <h2 class="title mb-2">Reported Disaster</h2>
+        <p class="subtitle mb-4 mb-sm-4 mb-lg-0">
+          List of reported disasters and their current status
         </p>
       </div>
       <div class="col-12 col-sm-12 col-lg-6">
-        <div class="row px-3">
-          <div class="col form-group mb-0 p-0">
-            <input
-              type="text"
-              class="form-control p-3"
-              placeholder="Search"
-              v-model="searchName"
-            />
-          </div>
+        <div class="row px-3 d-flex justify-content-end">
           <router-link
             class="ml-3 d-flex align-items-center"
-            :to="getUser && getUser.username ? '/reported-disasters/create' : '/login'"
+            to="/reported-disasters/create"
           >
             <button class="btn btn-red-reverse col px-3">Create a disaster report</button>
           </router-link>
@@ -35,6 +27,8 @@
               <th scope="col">Name</th>
               <th scope="col">Location</th>
               <th scope="col">Category</th>
+              <th scope="col">Status</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody v-for="disaster in getDisasters.data" :key="disaster.id">
@@ -43,6 +37,44 @@
               <td>{{ disaster.name }}</td>
               <td>{{ disaster.location.name }}</td>
               <td>{{ disaster.category }}</td>
+              <td>
+                <div :class="`btn-xs btn-${getColor(disaster.status)} d-inline py-2 px-3`">
+                  {{ disaster.status }}
+                </div>
+              </td>
+              <td>
+                <div
+                  class="btn-xs btn-purple cursor-pointer d-inline p-2 mr-1"
+                  @click="handleRedirect('view', disaster)"
+                >
+                  <img src="@/assets/img/view.png" />
+                </div>
+                <div
+                  class="btn-xs cursor-pointer d-inline p-2 mr-1"
+                  :class="disaster.status === 'Pending' ? 'btn-purple' : 'btn-light-grey'"
+                  @click="handleRedirect('edit', disaster)"
+                >
+                  <img src="@/assets/img/edit.png" />
+                </div>
+                <div
+                  class="btn-xs cursor-pointer d-inline p-2 mr-1"
+                  :class="disaster.status === 'Verified' ? 'btn-light-grey' : 'btn-purple'"
+                >
+                  <img src="@/assets/img/delete.png" />
+                </div>
+                <!-- <div
+                  class="btn-xs cursor-pointer d-inline p-2 mr-1"
+                  :class="disaster.status === 'Verified' ? 'btn-light-grey' : 'btn-green'"
+                >
+                  <img src="@/assets/img/verify.png" />
+                </div>
+                <div
+                  class="btn-xs cursor-pointer d-inline p-2"
+                  :class="disaster.status === 'Rejected' ? 'btn-light-grey' : 'btn-red'"
+                >
+                  <img src="@/assets/img/unverify.png" />
+                </div> -->
+              </td>
             </tr>
           </tbody>
         </table>
@@ -106,6 +138,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [-10, 125] },
           },
+          status: 'Verified',
           category: 'Flood',
           createdAt: '2012-07-14T01:00:00+01:00',
           updatedAt: '2012-07-14T01:00:00+01:00',
@@ -117,6 +150,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [-9, 120] },
           },
+          status: 'Pending',
           category: 'Flood',
           createdAt: '2012-07-14T01:00:00+01:00',
           updatedAt: '2013-07-14T01:00:00+01:00',
@@ -128,6 +162,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [-8, 110] },
           },
+          status: 'Pending',
           category: 'Earthquake',
           createdAt: '2012-07-14T01:00:00+01:00',
           updatedAt: '2012-07-13T01:00:00+01:00',
@@ -139,6 +174,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [-7, 115] },
           },
+          status: 'Verified',
           category: 'Tsunami',
           createdAt: '2012-07-14T01:00:00+01:00',
           updatedAt: '2020-05-13T01:00:00+01:00',
@@ -150,6 +186,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [-1, 135] },
           },
+          status: 'Verified',
           category: 'Wildfire',
           createdAt: '2012-07-14T01:00:00+01:00',
           updatedAt: '2012-07-04T01:00:00+01:00',
@@ -161,6 +198,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [1, 100] },
           },
+          status: 'Pending',
           category:
            'Wildfire',
           createdAt:
@@ -175,6 +213,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [3, 95] },
           },
+          status: 'Rejected',
           category:
            'Wildfire',
           createdAt:
@@ -189,6 +228,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [2, 97] },
           },
+          status: 'Rejected',
           category:
            'Wildfire',
           createdAt:
@@ -203,6 +243,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [0, 120] },
           },
+          status: 'Rejected',
           category:
            'Landslide',
           createdAt:
@@ -217,6 +258,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [1, 110] },
           },
+          status: 'Rejected',
           category:
            'Landslide',
           createdAt:
@@ -231,6 +273,7 @@ export default {
             name: 'Kec. Buahbatu, Bandung',
             map: { coordinates: [2, 127] },
           },
+          status: 'Pending',
           category:
            'Volcano',
           createdAt:
@@ -239,14 +282,30 @@ export default {
            '2012-07-14T01:00:00+01:00',
         },
       ],
+      statuses: [
+        { name: 'All Report', color: 'grey' },
+        { name: 'Verified', color: 'green' },
+        { name: 'Pending', color: 'yellow' },
+        { name: 'Rejected', color: 'red' },
+      ],
       limit: 10,
       searchName: '',
       page: 1,
     };
   },
   methods: {
+    getColor(status) {
+      return _.find(this.statuses, { name: status }).color;
+    },
     getReportedDate(date) {
       return utils.convertDate(new Date(date));
+    },
+    handleRedirect(method, disaster) {
+      if (method === 'view') {
+        this.$router.push(`/reported-disaster/${disaster.id}`);
+      } else if (method === 'edit' && disaster.status === 'Pending') {
+        this.$router.push(`/reported-disaster/${disaster.id}/edit`);
+      }
     },
   },
 };
